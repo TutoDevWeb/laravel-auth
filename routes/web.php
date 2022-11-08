8fac6dd1ee0c07cc2ab9d36d2106e4f43e3af000
TutoDevWeb <TutoDevWeb@outlook.fr>
2022-11-08 11:18:40 +0100

    Création d'un controller, de deux routes, de deux templates foo et bar.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/foo', [TestController::class, 'foo']);
+
+Route::get('/bar', [TestController::class, 'bar']);
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
